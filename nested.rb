6be7ec04628fe78a6582342c947dfc661821f096
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-23 20:01:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -93,7 +93,7 @@ array_new = ["LISP", "C"]
 
 programmer_hash[:yukihiro_matsumoto][:known_for] = 'Ruby'
 programmer_hash[:yukihiro_matsumoto][:languages] = array_new
-
+puts programmer_hash
 
 end
 
