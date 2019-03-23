65a04214d676e22ed05189580d0d02f65466544d
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-23 20:33:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -136,7 +136,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages] = 'Assembly'
+  programmer_hash[:dennis_ritchie][:languages] << 'Assembly'
   programmer_hash
 
 end
